e8fb0addebb11d4c7da1cf8e1bf9164abdecd691
Macy Bethel <mb310020@ohio.edu>
2024-09-04 17:35:25 -0400

    Lab 2 Done

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Macy Bethel
+ *        Date: 4/9/2024
+ * Description:Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
